9a48c5723706b789f4ad4971c2f602b38aa2184b
Sergio Cano <canosergio90@gmail.com>
2022-05-23 14:57:57 -0300

    hexagonal architecture layer structure
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

---
+++ src/main/java/io/github/sdev/buckpal/account/adapter/in/web/AccountController.java
@@ -0,0 +1,4 @@
+package io.github.sdev.buckpal.account.adapter.in.web;
+
+public class AccountController {
+}
---
+++ src/main/java/io/github/sdev/buckpal/account/domain/Account.java
@@ -0,0 +1,4 @@
+package io.github.sdev.buckpal.account.domain;
+
+public class Account {
+}
